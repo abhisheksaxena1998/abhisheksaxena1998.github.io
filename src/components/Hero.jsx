3458d7f38e13acbe4884740aaf414413fbf72c9c
Abhishek <abhi123923@gmail.com>
2023-06-04 21:47:01 +0530

    updated about

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a Software Engineer <br className='sm:block hidden' />
-            and I develop Fullstack web apps
+            and I develop Fullstack web applications.
           </p>
         </div>
       </div>

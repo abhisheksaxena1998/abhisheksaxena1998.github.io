f10e35119d6552aa7c95ef15d9195a0ebf747023
Abhishek <abhi123923@gmail.com>
2023-06-04 13:38:03 +0530

    added web view support

---
+++ src/assets/index.js
@@ -0,0 +1,82 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import movie_ticket_booking_system from "./movie_ticket_booking_system.png";
+import autonomous_indoor_drone from "./autonomous_indoor_drone.png";
+import chrome_extension from "./chrome_extension.png";
+import confusion_matrix from "./confusion_matrix.png";
+import malicious_url_detector from "./malicious_url_detector.png";
+import gunlaws_stance_detector from "./gunlaws_stance_detector.png";
+import content_analysis from "./content_analysis.png";
+import flair_detector from "./flair_detector.png";
+import voice_assistant from "./voice_assistant.png";
+import amex from "./amex.png";
+import flipkart_grid from "./flipkart_grid.png";
+import smart_india_hackathon from "./smart_india_hackathon.png";
+import technoutsav from "./technoutsav.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  movie_ticket_booking_system,
+  autonomous_indoor_drone,
+  chrome_extension,
+  confusion_matrix,
+  malicious_url_detector,
+  gunlaws_stance_detector,
+  content_analysis,
+  flair_detector,
+  voice_assistant,
+  technoutsav,
+  smart_india_hackathon,
+  flipkart_grid,
+  amex
+};
---
+++ src/components/Feedbacks.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
+import { redirect } from "react-router-dom";
+
+const FeedbackCard = ({
+  index,
+  testimonial,
+  name,
+  designation,
+  company,
+  image,
+  event,
+  repo_url,
+  redirect_url,
+  forks,
+  stars,
+  visitors,
+  points,
+}) => (
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className="bg-black-200 p-10 rounded-3xl xs:w-[] w-full"
+  >
+    <p className="text-white font-black text-[40px]">{event}</p>
+
+    <div className="mt-1">
+      <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
+
+      <div className="mt-7 flex justify-between items-center gap-1 flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row">
+        <div className="flex-4 flex flex-col px-3 py-1 gap-5">
+          {points &&
+            points.map((point) => (
+              <p className="mt-1 text-secondary text-[18px]" key={point}>
+                <span className="blue-text-gradient">●</span>
+                {point}
+              </p>
+            ))}
+
+          {repo_url && (
+            <>
+              <a href={redirect_url}>
+                <img
+                  src={repo_url}
+                  alt={`feedback_by-${name}`}
+                  className="w-[20rem]"
+                />
+              </a>
+              <span className="text-white font-medium text-[16px]">
+                <span className="blue-text-gradient">
+                  <a href={redirect_url}>
+                    Repository Statistics:
+                    <p className="flex flex-row py-4">
+                      {visitors && <img src={visitors} />}
+                      {stars && <img src={stars} />}
+                      {forks && <img src={forks} />}
+                    </p>
+                  </a>
+                </span>
+              </span>
+            </>
+          )}
+        </div>
+
+        <img
+          src={image}
+          alt={`feedback_by-${name}`}
+          className="w-[15rem] h-[15rem] rounded-full object-cover"
+        />
+      </div>
+    </div>
+  </motion.div>
+);
+
+const Feedbacks = () => {
+  return (
+    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+      <div
+        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}></p>
+          <h2 className={styles.sectionHeadText}>ACHIEVEMENTS.</h2>
+        </motion.div>
+      </div>
+      <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.event} index={index} {...testimonial} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Feedbacks, "");

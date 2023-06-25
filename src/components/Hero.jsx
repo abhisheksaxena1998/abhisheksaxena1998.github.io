d3f248853bfda8203db5dec1c317375465863af0
Abhishek <abhi123923@gmail.com>
2023-06-25 18:57:26 +0530

    updated UI animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,8 +12,18 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 50,
+              damping: 10,
+            }}
+          >
+            <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          </motion.div>
         </div>
 
         <div>

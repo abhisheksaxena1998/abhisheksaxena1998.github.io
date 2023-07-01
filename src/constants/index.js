3009b6931a19cf256ee1a27b8d00c0ab1735a088
Abhishek <abhi123923@gmail.com>
2023-07-01 13:09:06 +0530

    updated constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -126,20 +126,8 @@ const services = [
 
 const technologies = [
   {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
     name: "JavaScript",
     icon: javascript,
-  },
-  {
-    name: "TypeScript",
-    icon: typescript,
   },
   {
     name: "React JS",

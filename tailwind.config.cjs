23beec916236bfe201c7bb6449a29750abe4a63b
Abhishek <abhi123923@gmail.com>
2023-07-15 11:29:20 +0530

    updated mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
+  mode: "production",
   theme: {
     extend: {
       colors: {

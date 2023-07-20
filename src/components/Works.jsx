d59c5f6d9e43fffef7b51788a5431558867fc102
Abhishek <abhi123923@gmail.com>
2023-07-20 19:52:23 +0530

    updated dependencies

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -79,7 +79,7 @@ const ProjectCard = ({
           </div>
         </div>
 
-        <div>
+        <div className="mt-4">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
           {readme_link && (
             <a href={readme_link}>

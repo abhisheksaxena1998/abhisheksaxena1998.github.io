64d97d3b78efbc8822d182a36c29c36cd96c7c37
Abhishek <abhi123923@gmail.com>
2023-07-27 20:12:04 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   build: {
-    assetsInlineLimit: "2048", // 2kb
+    assetsInlineLimit: "4096",
     rollupOptions: {
       output: {
         manualChunks(id) {

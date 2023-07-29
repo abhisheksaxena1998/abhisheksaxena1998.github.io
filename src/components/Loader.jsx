0dd7456f43a73e86750ac915c0878c1a151227a6
Abhishek <abhi123923@gmail.com>
2023-07-29 09:44:18 +0530

    added theme to loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ const CanvasLoader = () => {
   const { progress } = useProgress();
   return (
     <Html
-      as='div'
+      as="div"
       center
       style={{
         display: "flex",
@@ -13,17 +13,8 @@ const CanvasLoader = () => {
         flexDirection: "column",
       }}
     >
-      <span className='canvas-loader'></span>
-      <p
-        style={{
-          fontSize: 14,
-          color: "#F1F1F1",
-          fontWeight: 800,
-          marginTop: 40,
-        }}
-      >
-        {progress.toFixed(2)}%
-      </p>
+      <span className="canvas-loader"></span>
+      <p className="loader-text">{progress.toFixed(2)}%</p>
     </Html>
   );
 };

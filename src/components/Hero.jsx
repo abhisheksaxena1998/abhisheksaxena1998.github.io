e622d60469805a87a2a22892d3d79ee2019f1cea
Abhishek <abhi123923@gmail.com>
2023-08-07 20:39:10 +0530

    removed && operator

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -72,10 +72,9 @@ const Hero = ({ isMobile }) => {
           </p>
         </div>
       </div>
-      && <ComputersCanvas />
-      )&&
+      <ComputersCanvas />)
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about" aria-label="Scroll button" >
+        <a href="#about" aria-label="Scroll button">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{

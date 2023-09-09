ea20584ab77ec58eca7d72a8c2c6aa02cff017e3
Abhishek <abhi123923@gmail.com>
2023-09-09 23:21:57 +0530

    simplified useeffect

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,21 +4,32 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
+const lightProps = {
+  hemisphere: {
+    intensity: 0.975,
+    groundColor: "black",
+  },
+  spot: {
+    position: [-20, 50, 10],
+    angle: 0.12,
+    penumbra: 1,
+    intensity: 1,
+    castShadow: true,
+    shadowMapSize: 1024,
+  },
+  point: {
+    intensity: 1.5,
+  },
+};
+
 const Computers = ({ isMobile }) => {
   const { scene } = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.975} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1.5} />
+      <hemisphereLight {...lightProps.hemisphere} />
+      <spotLight {...lightProps.spot} />
+      <pointLight {...lightProps.point} />
       <primitive
         object={scene}
         scale={isMobile ? 0.7 : 0.75}
@@ -33,24 +44,11 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
+    const handleMediaQueryChange = (event) => setIsMobile(event.matches);
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
+    return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
   }, []);
 
   return (
@@ -70,7 +68,6 @@ const ComputersCanvas = () => {
         />
         <Computers isMobile={isMobile} />
       </Suspense>
-
       <Preload all />
     </Canvas>
   );

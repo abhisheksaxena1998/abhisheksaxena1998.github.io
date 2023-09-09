0cb31a0b85cfc0fe1f7695fa03a13c03637d7c6f
Abhishek <abhi123923@gmail.com>
2023-09-10 00:31:36 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000, 
     minify: true,
     assetsInlineLimit: "4096",
     rollupOptions: {

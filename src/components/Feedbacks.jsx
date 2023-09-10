511758e0badf2624a50fc92b74d463bcfd5a07d6
Abhishek <abhi123923@gmail.com>
2023-09-10 14:24:46 +0530

    Used React.memo for AchievementCard

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,80 +6,84 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
-const AchievementCard = ({
-  index,
-  testimonial,
-  image,
-  event,
-  repo_url,
-  redirect_url,
-  forks,
-  stars,
-  visitors,
-  points,
-  isMobile,
-  award_text,
-  award_link,
-}) => (
-  <motion.div
-    variants={!isMobile && fadeIn("", "spring", index * 0.5, 0.75)}
-    className="bg-black-200 p-10 rounded-3xl xs:w-[] w-full"
-  >
-    <p className="text-white font-black text-[40px]">{event}</p>
+const AchievementCard = React.memo(
+  ({
+    index,
+    testimonial,
+    image,
+    event,
+    repo_url,
+    redirect_url,
+    forks,
+    stars,
+    visitors,
+    points,
+    isMobile,
+    award_text,
+    award_link,
+  }) => (
+    <motion.div
+      variants={!isMobile && fadeIn("", "spring", index * 0.5, 0.75)}
+      className="bg-black-200 p-10 rounded-3xl xs:w-[] w-full"
+    >
+      <p className="text-white font-black text-[40px]">{event}</p>
 
-    <div className="mt-1">
-      <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
-      {award_text && (
-        <a href={award_link}>
-          <div className="text-white tracking-wider text-[18px] mt-2">
-            <p className="award-text">{award_text}</p>
-          </div>
-        </a>
-      )}
+      <div className="mt-1">
+        <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
+        {award_text && (
+          <a href={award_link}>
+            <div className="text-white tracking-wider text-[18px] mt-2">
+              <p className="award-text">{award_text}</p>
+            </div>
+          </a>
+        )}
 
-      <div className="mt-7 flex justify-between items-center gap-1 flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row">
-        <div className="flex-4 flex flex-col px-3 py-1 gap-5">
-          {points &&
-            points.map((point) => (
-              <p className="mt-1 text-secondary text-[18px]" key={point}>
-                <span className="blue-text-gradient">●</span>
-                {point}
-              </p>
-            ))}
+        <div className="mt-7 flex justify-between items-center gap-1 flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row">
+          <div className="flex-4 flex flex-col px-3 py-1 gap-5">
+            {points &&
+              points.map((point) => (
+                <p className="mt-1 text-secondary text-[18px]" key={point}>
+                  <span className="blue-text-gradient">●</span>
+                  {point}
+                </p>
+              ))}
 
-          {repo_url && (
-            <>
-              <a href={redirect_url}>
-                <img
-                  src={repo_url}
-                  alt={`feedback_by-${index}`}
-                  className="w-[20rem]"
-                />
-              </a>
-              <span className="text-white font-medium text-[16px]">
-                <span className="blue-text-gradient">
-                  <a href={redirect_url}>
-                    Repository Statistics:
-                    <p className="flex flex-row py-4">
-                      {visitors && <img src={visitors} alt="github visitors" />}
-                      {stars && <img src={stars} alt="github stars" />}
-                      {forks && <img src={forks} alt="github forks" />}
-                    </p>
-                  </a>
+            {repo_url && (
+              <>
+                <a href={redirect_url}>
+                  <img
+                    src={repo_url}
+                    alt={`feedback_by-${index}`}
+                    className="w-[20rem]"
+                  />
+                </a>
+                <span className="text-white font-medium text-[16px]">
+                  <span className="blue-text-gradient">
+                    <a href={redirect_url}>
+                      Repository Statistics:
+                      <p className="flex flex-row py-4">
+                        {visitors && (
+                          <img src={visitors} alt="github visitors" />
+                        )}
+                        {stars && <img src={stars} alt="github stars" />}
+                        {forks && <img src={forks} alt="github forks" />}
+                      </p>
+                    </a>
+                  </span>
                 </span>
-              </span>
-            </>
-          )}
-        </div>
+              </>
+            )}
+          </div>
 
-        <img
-          src={image}
-          alt={`feedback_by-${index}`}
-          className="w-[15rem] h-[15rem] rounded-full object-cover"
-        />
+          <img
+            src={image}
+            alt={`feedback_by-${index}`}
+            className="w-[15rem] h-[15rem] rounded-full object-cover"
+          />
+        </div>
       </div>
-    </div>
-  </motion.div>
+    </motion.div>
+  )
 );
 
 const Achievements = () => {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
@@ -12,20 +12,19 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      if (scrollTop > 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
+  const handleScroll = useCallback(() => {
+    const scrollTop = window.scrollY;
+    if (scrollTop > 100) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
   return (
     <nav
@@ -127,5 +126,4 @@ const Navbar = () => {
     </nav>
   );
 };
-
 export default Navbar;

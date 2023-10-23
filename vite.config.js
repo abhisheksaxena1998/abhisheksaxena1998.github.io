62b2305239d3f035c402c7750a7d247bd43d22ae
Abhishek <abhi123923@gmail.com>
2023-10-23 19:05:33 +0530

    attempt to minify using terser

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
-    minify: true,
+    minify: "terser",
     assetsInlineLimit: "2048",
     rollupOptions: {
       output: {

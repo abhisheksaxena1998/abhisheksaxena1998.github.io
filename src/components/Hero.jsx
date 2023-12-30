b0b47030d18563a3980bac72909cc9f49ef931d6
Abhishek <abhi123923@gmail.com>
2023-12-30 23:56:14 +0530

    improved scrollbar's UI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,7 +80,7 @@ const Hero = memo(({ isMobile }) => {
       <ComputersCanvas />)
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about" aria-label="Scroll button">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[35px] h-[64px] rounded-3xl border-6 border-secondary flex justify-center items-start p-2 bg-transparent border border-gray-400">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -90,7 +90,7 @@ const Hero = memo(({ isMobile }) => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className="animate-pulse glow-dot w-3 h-3 rounded-full bg-secondary mb-1 "
             />
           </div>
         </a>

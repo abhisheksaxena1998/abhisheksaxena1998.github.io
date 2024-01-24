3d4e8f0a2a4fa5368c223e33b51a56bb72ea94bb
Abhishek <abhi123923@gmail.com>
2024-01-24 22:12:11 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compress from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [react(), compress()],
   build: {
     chunkSizeWarningLimit: 1000,
     minify: true,
     assetsInlineLimit: "1024",
+    assetFileNames: "assets/[name].[hash][ext]",
     rollupOptions: {
       output: {
         manualChunks(id) {

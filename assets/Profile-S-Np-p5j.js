f51131393ad6fe79053090c2fe0b7ffe789e61b9
Abhishek <abhi123923@gmail.com>
2024-04-26 15:13:11 +0530

    Updates

--- assets/Profile-S-Np-p5j.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Y5TUN0Pf.js";const e="/assets/linkedin-duqsNsp8.png",n=()=>s.jsx("div",{className:"profiles",children:s.jsx("a",{href:"https://www.linkedin.com/in/abhishek-saxena-5ba805180/",children:s.jsx("img",{loading:"lazy",src:e,alt:"Abhishek Saxena linkedin profile",className:"w-14 h-14 object-contain"})})});export{n as default};

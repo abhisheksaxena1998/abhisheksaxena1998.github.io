a6a13a7b7b7a582e5f54701a7d028f71ef6a971f
Abhishek <abhi123923@gmail.com>
2024-10-19 19:02:58 +0530

    ui issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,9 +46,7 @@ const Navbar = () => {
     <nav
       className={`${
         styles.paddingX
-      } w-full flex items-center py-5 fixed top-0 z-20 ${
-        scrolled ? "bg-primary" : "bg-transparent"
-      }`}
+      } w-full flex items-center py-5 fixed top-0 z-20 ${"bg-primary"}`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
